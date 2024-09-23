aff11ef643973ab1cef2a1a094e34fb19fbc88b9
Sibomana <bsibomana@leg.ne.gov>
2024-09-23 12:26:40 -0500

    Modified the postgresql uri

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Umwana11@localhost:5432/officeupdate_db'
+    #SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Umwana11@localhost:5432/officeupdate_db'
+    SQLALCHEMY_DATABASE_URI ="postgresql://u99m6b3d5banae:p3106e6e47e3204f2b212056bd0371c6f5af27b1b3e1ed54e355eddf1a07b11f3@c9mq4861d16jlm.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d67nqnqkod5i2n"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
